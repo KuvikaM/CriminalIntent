ff480252ac93e8dfa046a0c4eab5c23be6171b22
KuvikaM <kmgroup67@gmail.com>
2017-08-22 21:57:00 +0200

    add onClickListener

--- app/src/main/java/com/example/liongate/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/liongate/criminalintent/CrimeListFragment.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -43,7 +44,8 @@ public class CrimeListFragment extends Fragment {
         mCrimeRecyclerView.setAdapter(mAdapter);
     }
 
-    private class CrimeHolder extends RecyclerView.ViewHolder{
+    private class CrimeHolder extends RecyclerView.ViewHolder
+                                implements View.OnClickListener {
 
 
         private TextView mTitleTextView;
@@ -54,6 +56,7 @@ public class CrimeListFragment extends Fragment {
 
         public CrimeHolder(View itemView){
             super(itemView);
+            itemView.setOnClickListener(this);
             mTitleTextView = (TextView)itemView
                     .findViewById(R.id.list_item_crime_title_text_view);
             mDateTextView = (TextView) itemView
@@ -67,6 +70,13 @@ public class CrimeListFragment extends Fragment {
             mTitleTextView.setText(mCrime.getTitle());
             mDateTextView.setText(mCrime.getDate().toString());
             mSolvedCheckBox.setChecked(mCrime.isSolved());
+        }
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getActivity(),mCrime.getTitle() + " Clicked",Toast.LENGTH_SHORT)
+                    .show();
+            
         }
     }
 
