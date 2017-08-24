6b945d42a3d6b63e0e8bda9f66dd0bc6c159bfaf
KuvikaM <kmgroup67@gmail.com>
2017-08-24 14:01:25 +0200

    create ViewPager

---
+++ app/src/main/java/com/example/liongate/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,17 @@
+package com.example.liongate.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Liongate on 24.08.2017.
+ */
+
+public class CrimePagerActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+    }
+}
