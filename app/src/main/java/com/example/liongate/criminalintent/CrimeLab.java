0850310fdc4f108e67eac9f5a21304c718bd17cb
KuvikaM <kmgroup67@gmail.com>
2017-08-25 16:34:43 +0200

    add Menu to Activity

--- app/src/main/java/com/example/liongate/criminalintent/CrimeLab.java
+++ app/src/main/java/com/example/liongate/criminalintent/CrimeLab.java
@@ -16,14 +16,9 @@ public class CrimeLab {
     private List<Crime> mCrimes;
 
     private CrimeLab(Context context){
+
         mCrimes = new ArrayList<>();
-        for (int i =0 ; i<100 ; i++){
-            Crime crime = new Crime();
-            crime.setTitle("Crime # "+i);
-            crime.setSolved(i%2 ==0);
-            mCrimes.add(crime);
         }
-    }
 
     public static CrimeLab get(Context context){
 
@@ -32,6 +27,9 @@ public class CrimeLab {
         }
 
         return sCrimeLab;
+    }
+    public void addCrime(Crime crime){
+        mCrimes.add(crime);
     }
 
     public List<Crime> getCrimes() {
--- app/src/main/java/com/example/liongate/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/liongate/criminalintent/CrimeListFragment.java
@@ -7,6 +7,9 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
@@ -27,6 +30,12 @@ public class CrimeListFragment extends Fragment {
     private CrimeAdapter mAdapter;
     private int mCurrentPosition;
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -46,6 +55,26 @@ public class CrimeListFragment extends Fragment {
         super.onResume();
         updateUI();
         //updateUI(mCurrentPosition);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_crime_list,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_new_crime:
+                Crime crime = new Crime();
+                CrimeLab.get(getActivity()).addCrime(crime);
+                Intent intent = CrimePagerActivity.newIntent(getActivity(), crime.getId());
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void updateUI(){
